71d0efad7f86525aea07037c22f3e127676c3453
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2021-08-11 10:18:42 -0500

    nav is only responsive

---
+++ src/scripts/script.js
@@ -0,0 +1,26 @@
+let header_main = document.querySelector('.header-main');
+
+let navmenu = document.querySelector('.navmenu-container');
+let btn_navmenu = document.querySelector('.btn-navmenu');
+
+btn_navmenu.addEventListener('click', e => {
+    e.stopPropagation();
+    toggleMenu(e);
+});
+header_main.addEventListener('click', e => {
+    close_menu(e);
+});
+
+const toggleMenu = e => {
+    if(!navmenu.classList.contains('active')) open_menu(e);
+    else close_menu(e);
+}
+
+const open_menu = () => {
+    navmenu.classList.add('active');
+    btn_navmenu.classList.add('active');
+};
+const close_menu = () => {
+    navmenu.classList.remove('active');
+    btn_navmenu.classList.remove('active');
+};
