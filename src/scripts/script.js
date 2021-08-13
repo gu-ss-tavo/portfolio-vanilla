a90e6f1416e230c6fe52bd65c11f25cca2cd9eb2
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2021-08-13 00:09:26 -0500

    nav update 1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,30 +3,28 @@ let header_main = document.querySelector('.header-main');
 let navmenu = document.querySelector('.navmenu-container');
 let btn_navmenu = document.querySelector('.btn-navmenu');
 
-btn_navmenu.addEventListener('click', e => {
+// * events
+window.onresize = () => {
+    if(innerWidth >= 576) {
+        close_menu();
+    }
+};
+
+// * nav menu
+btn_navmenu.onclick =  e => {
     e.stopPropagation();
     toggleMenu(e);
-});
-header_main.addEventListener('click', e => {
+};
+header_main.onclick = e => {
     close_menu(e);
-});
+};
 
 const toggleMenu = e => {
-    if(!navmenu.classList.contains('active')) open_menu(e);
-    else close_menu(e);
-}
-
-const open_menu = () => {
-    navmenu.classList.add('active');
-    btn_navmenu.classList.add('active');
+    navmenu.classList.toggle('active');
+    btn_navmenu.classList.toggle('active');
 };
-const close_menu = () => {
+const close_menu = e => {
     navmenu.classList.remove('active');
     btn_navmenu.classList.remove('active');
 };
 
-window.addEventListener('resize', () => {
-    if(innerWidth >= 576) {
-        close_menu();
-    }
-})
