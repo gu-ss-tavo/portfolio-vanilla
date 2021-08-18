9697727ea9cc6da122d30a1bea756cb19754cc49
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2021-08-17 22:22:27 -0500

    nav update 3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,8 @@
-let header_main = document.querySelector('.header-main');
-
 let navmenu = document.querySelector('.navmenu-container');
 let navmenu_list = document.querySelector('.navmenu-list');
 let btn_navmenu = document.querySelector('.btn-navmenu');
+
+let articles = document.querySelector('.section-container').children;
 
 // * events
 window.onresize = () => {
@@ -13,6 +13,19 @@ window.onresize = () => {
 window.onscroll= () => {
     if(window.scrollY > 0) navmenu_list.classList.add("active");
     else navmenu_list.classList.remove("active");
+
+    if(window.scrollY < articles[1].offsetTop) {
+        console.log('hola');
+        activeItem(0);
+    }else if(window.scrollY < articles[2].offsetTop-10) {
+        activeItem(1);
+    }else if(window.scrollY < articles[3].offsetTop-10) {
+        activeItem(2);
+    }else if(window.scrollY < articles[4].offsetTop-10) {
+        activeItem(3);
+    }else {
+        activeItem(4);
+    }
 };
 
 // * nav menu
@@ -33,3 +46,12 @@ const close_menu = e => {
     navmenu.classList.remove('active');
     btn_navmenu.classList.remove('active');
 };
+const activeItem = index => {
+    for (let i = 0; i < navmenu_list.children.length; i++) {
+        if(index == i) {
+            navmenu_list.children[i].classList.add('active');
+            continue;
+        }
+        navmenu_list.children[i].classList.remove('active');
+    }
+}
